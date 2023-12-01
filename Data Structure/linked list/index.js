00892b1852948cb5899aaa9f13363a319ca082cb
Vinayak <singhvinayak7111@gmail.com>
2023-12-01 10:30:59 +0530

    first commit

---
+++ Data Structure/linked list/index.js
@@ -0,0 +1,61 @@
+// How to start learning Data Structures?
+// 1. Learn about Complexities
+// 2. Learn Data Structures
+
+// Learn Data Structures
+
+// 1. Array in javascript
+// 2. String in Javascript
+// 3.  Linked List in Javascript
+// 4. Stack in Javascript
+// 5. Queue in Javascript
+// 6. Tree in Javascript
+// 7. Priority Queue in Javascript
+// 8. Map in Javascript
+// 9.  Set in Javascript
+// 10. Graph in Javascript
+
+//----- Array in javascript---
+
+// Initializing while declaring
+// Creates an array having elements 10, 20, 30, 40, 50
+let house = new Array(10, 20, 30, 40, 50);
+
+// Creates an array of 5 undefined elements
+let house1 = new Array(5);
+
+// Creates an array with element 1BHK
+let home = new Array("1BHK");
+// console.log(house);
+// console.log(house1);
+// console.log(home);
+
+//-----String in javascript---
+
+
+
+// String written inside quotes
+var x = "Welcome to GeeksforGeeks!";
+console.log(x);
+ 
+  // Declare an object
+var y = new String("Great Geek");
+console.log(y);
+
+
+let a = "abcdefgh";
+
+// Finding the first index of the character 'b'
+console.log(a.indexOf('b'));
+
+
+let a2 = "Hello World";
+
+let arrString = ["Geeks", "for", "Geeks"]
+
+// Replacing the word 'World' with 'Geeks'
+console.log(a2.replace("World", arrString[0]));
+
+
+// -----Linked List in Javascript----
+
